0b770f2a79c547f8659c8f08fd144c5c506aba6b
Anemois <46663053+Anemois@users.noreply.github.com>
2023-09-26 21:21:13 +0800

    Create 1001 - Hello World!.cpp

---
+++ 1001 - Hello World!.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define Anemoi ios_base::sync_with_stdio(false);cin.tie(0);
+
+using namespace std;
+ 
+typedef long long ll;
+
+int main() {
+    Anemoi;
+	cout << "Hello Tmt World XD!";
+}
